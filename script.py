98b2524c9f825a6551206948e850ed812bb9cbd9
Mustakim Ahmed Hasan <mustakimahmedhasan@Mustakims-MacBook-Air.local>
2023-09-25 00:15:04 -0400

    init

---
+++ script.py
@@ -0,0 +1,26 @@
+import ssl
+import smtplib
+import maskpass
+from email.message import EmailMessage
+
+sender = 'ninjacarrot9@gmail.com'
+
+print(f'From: {sender}')
+
+password = maskpass.askpass(prompt="Enter your Password: ", mask="*") 
+receiver = input('Send to: ')
+subject = input('Subject: ')
+body= input('Body: ')
+
+email = EmailMessage()
+
+email['From'] = sender
+email['To'] = receiver
+email['subject'] = subject
+email.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(sender, password)
+    smtp.sendmail(sender, receiver, email.as_string())
